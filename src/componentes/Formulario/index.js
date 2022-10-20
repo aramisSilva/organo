8a3019cacd9ef2aebab7e7b31efdc33d710cde4c
Aramís Ocrécio da Silva <aods@ic.ufal.br>
2022-10-20 12:09:38 -0300

    finalização do layout do formulário de adição

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,17 +1,29 @@
 import React from "react";
 import "./Formulario.css";
-const Formulario = (props) => {
-  console.log(props);
+import CampoTexto from "../CampoTexto";
+const Formulario = () => {
   return (
     <>
-      <div className="campo-texto">
+      <section className="formulario">
         <form>
-          <div>
-            <label>{props.label}</label>
-            <input placeholder={props.placeholder} />
-          </div>
+          <h2 className="titulo">
+            Preencha os dados para criar o card do colaborador.
+          </h2>
+          <CampoTexto
+            label="Atividade"
+            placeholder="Digite o que você irá estudar"
+          />
+          <CampoTexto
+            label="Disciplina"
+            placeholder="Em qual disciplina eu me enquadro"
+          />
+          <CampoTexto
+            label="Imagem"
+            placeholder="Informe o endereço da imagem"
+          />
+          <CampoTexto label="Time" />
         </form>
-      </div>
+      </section>
     </>
   );
 };
