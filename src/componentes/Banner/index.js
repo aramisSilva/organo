9d26182343ff49402659bc6bafb25217e0e81f04
Aramís Ocrécio da Silva <aods@ic.ufal.br>
2022-10-20 10:39:54 -0300

    innitial commit

---
+++ src/componentes/Banner/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Banner.css";
+const Banner = () => {
+  return (
+    <header className="banner ">
+      <img
+        src="/imagens/banner.png"
+        alt="Banner principal da página do Organo"
+      ></img>
+    </header>
+  );
+};
+
+export default Banner;
